f46c67569a600f1f0fca4b07f71c15c7c8565a98
Tiago Mazzon <tiago.mazzon@gmail.com>
2022-11-07 09:17:55 -0300

    classe playlist com iteração interna automática

--- modelo.py
+++ modelo.py
@@ -22,6 +22,7 @@ class Programa:
     def __str__(self):
         return f'{self._nome} - {self.ano} - {self._likes} Likes'
 
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
@@ -40,17 +41,47 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
-vingadores.dar_like()
-vingadores.dar_like()
-vingadores.dar_like()
-
 atlanta = Serie("atlanta", 2018, 2)
-atlanta.nome = 'atlanta - de glover'
+tmep = Filme("Todo mundo em pânico", 1999, 100)
+demolidor = Serie("Demolidor", 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
+
+print(playlist_fim_de_semana[0])
