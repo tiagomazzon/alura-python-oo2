70262d3e80648fb70515de4c1382411c3ef9d9a7
Tiago Mazzon <tiago.mazzon@gmail.com>
2022-11-17 11:06:19 -0300

    feat: started url extractor

---
+++ extrator-url/main.py
@@ -0,0 +1,9 @@
+#url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = "bytebank.com/cambio?moedaOrigem=real"
+print(url)
+
+url_base = url[0:19]
+print(url_base)
+
+url_parametros = url[20:36]
+print(url_parametros)
